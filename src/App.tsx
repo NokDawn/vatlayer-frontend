89408a057684b9f2636066f999ef8497c1004dda
Mateusz Wojtas <nookdawn@gmail.com>
2023-03-26 12:37:49 +0200

    feat: Prevent using +/- operators in input

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
 const schema = yup.object({
-  nip: yup.number().min(1000000000).max(9999999999).required(),
+  nip: yup
+    .number()
+    .min(1000000000, 'Numer NIP musi mieć 10 cyfr')
+    .max(9999999999, 'Numer NIP musi mieć 10 cyfr')
+    .typeError('Numer NIP musi być liczbą')
+    .required('Numer NIP jest wymagany'),
 });
 
 type FormData = yup.InferType<typeof schema>;
@@ -13,32 +18,45 @@ function App() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<FormData>({
     resolver: yupResolver(schema),
   });
-  const onSubmit = (data: FormData) => console.log(data);
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
 
-  console.log(watch('nip'));
+  const isNipError = errors.nip;
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-control w-full max-w-xs">
           <label htmlFor="nip" className="label">
-            <span className="label-text">Numer NIP</span>
+            <span className="label-text">Podaj numer NIP</span>
           </label>
           <input
             id="nip"
-            type="text"
-            placeholder="Type here"
-            className={`input input-bordered w-full max-w-xs mb-2 ${
-              errors.nip && 'input-error'
+            type="number"
+            placeholder="np. 1234567890"
+            className={`input input-bordered w-full max-w-xs ${
+              isNipError && 'input-error'
             }`}
+            onKeyDown={e => {
+              if (e.key === '-' || e.key === '+') {
+                e.preventDefault();
+              }
+            }}
             {...register('nip')}
           />
-          <button className="btn btn-primary">Pobierz dane</button>
+          {isNipError && (
+            <label className="label">
+              <span className="label-text-alt text-red-500">
+                {errors.nip?.message || 'Błąd'}
+              </span>
+            </label>
+          )}
+          <button className="btn btn-primary mt-2">Pobierz dane</button>
         </div>
       </form>
     </div>
